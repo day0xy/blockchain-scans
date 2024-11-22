89769373ceea3d58554050fdd3c7caefba6e111a
day0xy <jasonfan1337@gmail.com>
2024-11-22 19:21:47 +0800

    1

--- background.js
+++ background.js
@@ -4,6 +4,11 @@ chrome.runtime.onInstalled.addListener(() => {
     title: "Check Address on Etherscan",
     contexts: ["selection"],
   });
+  chrome.contextMenus.create({
+    id: "checkSolscan",
+    title: "Check Address on Solscan",
+    contexts: ["selection"],
+  });
 });
 
 chrome.contextMenus.onClicked.addListener((info) => {
@@ -18,4 +23,17 @@ chrome.contextMenus.onClicked.addListener((info) => {
       alert("Selected text is not a valid Ethereum address.");
     }
   }
+
+  if (info.menuItemId === "checkSolscan") {
+    const selectedText = info.selectionText.trim();
+    const solAddressRegex = /^([A-Za-z0-9]{44})$/;
+
+    if (solAddressRegex.test(selectedText)) {
+      const url = `https://solscan.io/address/${selectedText}`;
+      chrome.tabs.create({ url });
+    } else {
+      alert("Selected text is not a valid Solana address.");
+    }
+  }
+
 });
